653f1ee66740f0e6dbcd81a930025d9a782b5a0e
Patelvivek24 <vivek@inventam.com>
2026-02-27 14:34:48 +0530

    Use Lazy Loading for Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'wowjs';
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Contact from './pages/Contect/Contact';
 import LiquidFilters from './components/common/LiquidFilters';
 import FloatingContactButtons from './components/common/FloatingContactButtons/FloatingContactButtons';
+import Preloader from './components/common/Preloader';
 import './assets/styles/App.css';
 import './assets/styles/index.css';
 import './assets/styles/fonts.css';
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const About = lazy(() => import('./pages/About/About'));
+const Contact = lazy(() => import('./pages/Contect/Contact'));
 
 function App() {
   useEffect(() => {
@@ -24,12 +26,14 @@ function App() {
     <Router>
       <LiquidFilters />
       <FloatingContactButtons />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        {/* Add more routes as needed */}
-      </Routes>
+      <Suspense fallback={<Preloader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
