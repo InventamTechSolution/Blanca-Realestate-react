2f4bac38d99f8c1c425df654b920e4ad9594b2d8
Patelvivek24 <vivek@inventam.com>
2026-03-03 12:46:40 +0530

    refactor App component to remove lazy loading for routes, improving initial load performance and simplifying structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import LiquidFilters from "./components/common/LiquidFilters";
 import FloatingContactButtons from "./components/common/FloatingContactButtons/FloatingContactButtons";
-import Preloader from "./components/common/Preloader";
+
 import "./assets/styles/App.css";
 import "./assets/styles/index.css";
 import "./assets/styles/fonts.css";
 
-const Home = lazy(() => import("./pages/Home"));
-const About = lazy(() => import("./pages/About"));
-const Contact = lazy(() => import("./pages/Contect"));
-const Registration = lazy(() => import("./pages/Registration"));
-const Projects = lazy(() => import("./pages/Projects"));
-const ProjectDetails = lazy(() => import("./pages/ProjectDetails"));
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contect";
+import Registration from "./pages/Registration";
+import Projects from "./pages/Projects";
+import ProjectDetails from "./pages/ProjectDetails";
 
 function App() {
   // useEffect(() => {
@@ -25,17 +24,14 @@ function App() {
     <>
       <LiquidFilters />
       <FloatingContactButtons />
-      <Suspense fallback={<Preloader />}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/project/:id" element={<ProjectDetails />} />
-          {/* Add more routes as needed */}
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/project/:id" element={<ProjectDetails />} />
+      </Routes>
     </>
   );
 }
