68b02ee40e03dc1c1c2b9aed7a8e7dae1d476147
Patelvivek24 <vivek@inventam.com>
2026-03-03 10:05:38 +0530

    working on a statbadge error

--- src/components/common/StatBadge/StatBadge.jsx
+++ src/components/common/StatBadge/StatBadge.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import './StatBadge.css';
+// import './StatBadge.css';
 
 const StatBadge = ({ count, text, className = "" }) => {
     return (
-        <div className={`hero-stat-box ${className}`}>
-            <div className="stat-number" data-count={count}>0</div>
-            <div className="stat-text">{text}</div>
+        // <div className={`hero-stat-box ${className}`}>
+        //     <div className="stat-number" data-count={count}>0</div>
+        //     <div className="stat-text">{text}</div>
+        // </div>
+        <div>
+            <div data-count={count}>0</div>
+            <div>{text}</div>
         </div>
     );
 };
