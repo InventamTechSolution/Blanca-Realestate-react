000d527744e7a067b766101a4687cfe802550189
Patelvivek24 <vivek@inventam.com>
2026-03-16 15:03:40 +0530

    add link in about us menu in header

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -17,10 +17,20 @@ const About = () => {
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        window.scrollTo(0, 0);
+        if (!window.location.hash) {
+            window.scrollTo(0, 0);
+        }
+        
         const handleLoad = () => {
             setTimeout(() => {
                 setIsLoading(false);
+                if (window.location.hash) {
+                    const id = window.location.hash.replace('#', '');
+                    const element = document.getElementById(id);
+                    if (element) {
+                        element.scrollIntoView({ behavior: 'smooth' });
+                    }
+                }
             }, 800);
         };
 
