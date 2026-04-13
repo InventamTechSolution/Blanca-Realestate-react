a987852c7081df82e8b7cff208b4815a6d2f9b19
Mihir <mihir@inventam.com>
2026-04-13 19:39:42 +0530

    change fev icon

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import AppShell from "./AppShell";
 import Header from "@/components/layout/Header/Header";
 import Footer from "@/components/layout/Footer/Footer";
 import GoogleAnalyticsScript from "@/components/scripts";
-
+import logo from '@'
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 export const fetchCache = "force-no-store";
@@ -17,8 +17,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/images/logos/favicon.png" type="image/png" />
-        <link rel="shortcut icon" href="/images/logos/favicon.png" type="image/png" />
+        <link rel="icon" href="./favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="./favicon.png" type="image/png" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
